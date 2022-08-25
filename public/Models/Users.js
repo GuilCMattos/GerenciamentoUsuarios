aab9a042943d8d07ce0d621c221b523c8ace2363
Guilherme Mattos <91619478+GuilCMattos@users.noreply.github.com>
2022-08-25 08:23:06 -0300

    Refec fetch for api (#2)
    * add: AJAX to catch informations of DB
    
    * add: httpRequest to facility
    
    * add: method post working in view
    
    * add: button remove works in database
    
    * refec: remove createNewId
    
    * refec: add limit in images upload
    
    * add: fetch to facility

--- public/Models/Users.js
+++ public/Models/Users.js
@@ -81,7 +81,8 @@ class User {
 
     static getUsersStorage() { 
 
-       return HttpRequest.get('/users')
+       return Fetch.get('/users')
+
 
     }
 
@@ -108,10 +109,12 @@ class User {
 
             if(this.id) { 
 
-                promise = HttpRequest.put(`/users/${this.id}`, this.toJSON())
+
+                promise = Fetch.put(`/users/${this.id}`, this.toJSON())
 
             } else { 
-                promise = HttpRequest.post(`/users`, this.toJSON())
+                promise = Fetch.post(`/users`, this.toJSON())
+
             }
 
             promise.then(data=> { 
@@ -132,7 +135,9 @@ class User {
 
     remove(){
      
-        return HttpRequest.delete(`/users/${this.id}`);
+
+        return Fetch.delete(`/users/${this.id}`);
+
     }
 
 }
